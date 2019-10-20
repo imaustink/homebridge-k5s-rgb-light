5cc2a20b5cafe1588828f3413f77186f8ebce68c
imaustink <austin@austinkurpuis.com>
2019-10-20 08:36:20 -0700

    Fix more typos

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const getK5SFanController = require('./homebridge-plugin');
+const getK5SRGBLight = require('./homebridge-plugin');
 
 module.exports = function (homebridge) {
-  const K5SRGBLight = getK5SFanController(homebridge.hap.Service, homebridge.hap.Characteristic);
-  homebridge.registerAccessory('K5S-light-controller', 'K5SRGBLight', K5SRGBLight);
+  const K5SRGBLight = getK5SRGBLight(homebridge.hap.Service, homebridge.hap.Characteristic);
+  homebridge.registerAccessory('K5S-RGB-light', 'K5SRGBLight', K5SRGBLight);
 };
